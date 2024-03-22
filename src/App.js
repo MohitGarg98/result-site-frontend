148741b8bc67daa30e8dc687577b5f2f12675e09
mohitgarg98 <gargmohit29525@gmail.com>
2024-03-23 02:30:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [allUsersData, setAllUsersData] = useState([]);
+
+  const getUserData = async () => {
+    const userData = await axios.get('https://result-site.onrender.com/example/user-data');
+    setAllUsersData(userData.data)
+    console.log('userData', userData.data)
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        allUsersData.map(data => {
+          return <h3>User Name is {data.name}</h3>
+        })
+      }
     </div>
   );
 }
