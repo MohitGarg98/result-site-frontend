13d25164bae639ab43206a4b8f1660403a48a033
mohitgarg98 <gargmohit29525@gmail.com>
2024-03-30 22:13:39 +0530

    download link

--- src/StudentResultsTable.js
+++ src/StudentResultsTable.js
@@ -1,14 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 import "./StudentResultsTable.css";
 
 const StudentResultsTable = ({ studentResult }) => {
   const pdfRef = useRef();
+  const [isPdfDownloading, setIsPdfDownloading] = useState(false);
 
   const downloadPDF = () => {
     const input = pdfRef.current;
-
+    setIsPdfDownloading(true);
     html2canvas(input).then((canvas) => {
       const imgData = canvas.toDataURL("image/png");
 
@@ -36,7 +37,8 @@ const StudentResultsTable = ({ studentResult }) => {
         imgWidth * ratio,
         imgHeight * ratio
       );
-      pdf.save("invoice.pdf");
+      pdf.save("result.pdf");
+      setIsPdfDownloading(false);
     });
   };
 
@@ -49,103 +51,109 @@ const StudentResultsTable = ({ studentResult }) => {
 
   return (
     <>
-    <div className="result-container" ref={pdfRef}>
-      <h1>Dr. Kalam Coaching Classes</h1>
-      <h2>Scholarship Result</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <strong>Name:</strong>
-            </td>
-            <td>{studentResult["CANDIDATE NAME"]}</td>
-            <td>
-              <strong>Roll No:</strong>
-            </td>
-            <td>{studentResult["ROLL NO"]}</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>Father's Name:</strong>
-            </td>
-            <td>{studentResult["FATHER'S NAME"]}</td>
-            <td>
-              <strong>School Name:</strong>
-            </td>
-            <td>{studentResult["SCHOOL NAME"]}</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>Class:</strong>
-            </td>
-            <td>{studentResult["CLASS"]}</td>
-            <td>
-              <strong>Medium:</strong>
-            </td>
-            <td>{studentResult["MEDIUM"]}</td>
-          </tr>
-        </tbody>
-      </table>
-      <div>
+      <div className="result-container" ref={pdfRef}>
+        <h1>Dr. Kalam Coaching Classes</h1>
+        <h2>Scholarship Result</h2>
         <table>
-          <thead>
-            <tr>
-              <th>Subject</th>
-              <th>Obtained Marks</th>
-              <th>Maqx Marks</th>
-            </tr>
-          </thead>
           <tbody>
             <tr>
-              <td>SECTION - A</td>
-              <td>{50}</td>
-              <td>{studentResult["SECTION - A"] ?? 0}</td>
-            </tr>
-
-            <tr>
-              <td>SECTION - B</td>
-              <td>{50}</td>
-              <td>{studentResult['SECTION - B'] ?? 0}</td>
-            </tr>
-
-            <tr>
-              <td>SECTION - C</td>
-              <td>{50}</td>
-              <td>{studentResult['SECTION - C'] ?? 0}</td>
-            </tr>
-
-            <tr>
-              <td>SECTION - D</td>
-              <td>{50}</td>
-              <td>{studentResult['SECTION - D'] ?? 0}</td>
-            </tr>
-
-            <tr>
-              <td>SECTION - E</td>
-              <td>{50}</td>
-              <td>{studentResult['SECTION - E'] ?? 0}</td>
-            </tr>
-
-
-            <tr>
               <td>
-                <strong>Total Marks:</strong>
+                <strong>Name:</strong>
               </td>
-              <td>{250}</td>
-              <td>{totalMarks ? totalMarks : 0}</td>
+              <td>{studentResult["CANDIDATE NAME"]}</td>
+              <td>
+                <strong>Roll No:</strong>
+              </td>
+              <td>{studentResult["ROLL NO"]}</td>
             </tr>
             <tr>
               <td>
-                <strong>Rank:</strong>
+                <strong>Father's Name:</strong>
               </td>
-              <td colSpan="2">-</td>
+              <td>{studentResult["FATHER'S NAME"]}</td>
+              <td>
+                <strong>School Name:</strong>
+              </td>
+              <td>{studentResult["SCHOOL NAME"]}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Class:</strong>
+              </td>
+              <td>{studentResult["CLASS"]}</td>
+              <td>
+                <strong>Medium:</strong>
+              </td>
+              <td>{studentResult["MEDIUM"]}</td>
             </tr>
           </tbody>
         </table>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Subject</th>
+                <th>Obtained Marks</th>
+                <th>Max Marks</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>SECTION - A</td>
+                <td>{50}</td>
+                <td>{studentResult["SECTION - A"] ?? 0}</td>
+              </tr>
+
+              <tr>
+                <td>SECTION - B</td>
+                <td>{50}</td>
+                <td>{studentResult['SECTION - B'] ?? 0}</td>
+              </tr>
+
+              <tr>
+                <td>SECTION - C</td>
+                <td>{50}</td>
+                <td>{studentResult['SECTION - C'] ?? 0}</td>
+              </tr>
+
+              <tr>
+                <td>SECTION - D</td>
+                <td>{50}</td>
+                <td>{studentResult['SECTION - D'] ?? 0}</td>
+              </tr>
+
+              <tr>
+                <td>SECTION - E</td>
+                <td>{50}</td>
+                <td>{studentResult['SECTION - E'] ?? 0}</td>
+              </tr>
+
+
+              <tr>
+                <td>
+                  <strong>Total Marks:</strong>
+                </td>
+                <td>{250}</td>
+                <td>{totalMarks ? totalMarks : 0}</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Rank:</strong>
+                </td>
+                <td colSpan="2">-</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-    <a onClick={downloadPDF}>Download</a>
-  </>
+      <div className="download-link">
+        {isPdfDownloading ? (
+          <span>Downloading...</span>
+        ) : (
+          <a onClick={downloadPDF}>Download Result</a>
+        )}
+      </div>
+    </>
   );
 };
 
