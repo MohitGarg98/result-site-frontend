1b6f4563cbaef00e4bbc81f88f03c48b3ff924e0
mohitgarg98 <gargmohit29525@gmail.com>
2024-03-30 17:45:47 +0530

    pdf generate

--- src/StudentResultsTable.js
+++ src/StudentResultsTable.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import "./StudentResultsTable.css";
 
 const StudentResultsTable = ({ studentResult }) => {
-  const convertToPDF = () => {
-    window.print(); // Opens the browser's print dialog
+  const pdfRef = useRef();
+
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+
+      const pdf = new jsPDF("p", "mm", "a4", true);
+
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+
+      const imgWidth = canvas.width;
+
+      const imgHeight = canvas.height;
+
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+
+      const imgY = 30;
+
+      pdf.addImage(
+        imgData,
+        "PNG",
+        imgX,
+        imgY,
+        imgWidth * ratio,
+        imgHeight * ratio
+      );
+      pdf.save("invoice.pdf");
+    });
   };
-  const totalMarks = (Number(studentResult['SECTION - A']) ?? 0) + 
-    (Number(studentResult['SECTION - B']) ?? 0) + 
-    (Number(studentResult['SECTION - C']) ?? 0) +
-    (Number(studentResult['SECTION - D']) ?? 0) + 
-    (Number(studentResult['SECTION - E']) ?? 0);
+
+  const totalMarks =
+    (Number(studentResult["SECTION - A"]) ?? 0) +
+    (Number(studentResult["SECTION - B"]) ?? 0) +
+    (Number(studentResult["SECTION - C"]) ?? 0) +
+    (Number(studentResult["SECTION - D"]) ?? 0) +
+    (Number(studentResult["SECTION - E"]) ?? 0);
+
   return (
-    <div className="result-container">
+    <>
+    <div className="result-container" ref={pdfRef}>
       <h1>Dr. Kalam Coaching Classes</h1>
       <h2>Scholarship Result</h2>
       <table>
@@ -20,11 +58,11 @@ const StudentResultsTable = ({ studentResult }) => {
             <td>
               <strong>Name:</strong>
             </td>
-            <td>{studentResult['CANDIDATE NAME']}</td>
+            <td>{studentResult["CANDIDATE NAME"]}</td>
             <td>
               <strong>Roll No:</strong>
             </td>
-            <td>{studentResult['ROLL NO']}</td>
+            <td>{studentResult["ROLL NO"]}</td>
           </tr>
           <tr>
             <td>
@@ -34,17 +72,17 @@ const StudentResultsTable = ({ studentResult }) => {
             <td>
               <strong>School Name:</strong>
             </td>
-            <td>{studentResult['SCHOOL NAME']}</td>
+            <td>{studentResult["SCHOOL NAME"]}</td>
           </tr>
           <tr>
             <td>
               <strong>Class:</strong>
             </td>
-            <td>{studentResult['CLASS']}</td>
+            <td>{studentResult["CLASS"]}</td>
             <td>
               <strong>Medium:</strong>
             </td>
-            <td>{studentResult['MEDIUM']}</td>
+            <td>{studentResult["MEDIUM"]}</td>
           </tr>
         </tbody>
       </table>
@@ -53,17 +91,15 @@ const StudentResultsTable = ({ studentResult }) => {
           <thead>
             <tr>
               <th>Subject</th>
-              <th>Max Marks</th>
               <th>Obtained Marks</th>
+              <th>Maqx Marks</th>
             </tr>
           </thead>
           <tbody>
-
-
             <tr>
               <td>SECTION - A</td>
               <td>{50}</td>
-              <td>{studentResult['SECTION - A'] ?? 0}</td>
+              <td>{studentResult["SECTION - A"] ?? 0}</td>
             </tr>
 
             <tr>
@@ -107,8 +143,9 @@ const StudentResultsTable = ({ studentResult }) => {
           </tbody>
         </table>
       </div>
-      <button onClick={convertToPDF}>Convert to PDF</button>
     </div>
+    <a onClick={downloadPDF}>Download</a>
+  </>
   );
 };
 
